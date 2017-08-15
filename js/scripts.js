b8bf2b3c80c3a47d4c649c8422283c3c59948711
Dawson Mortenson and John Parsons <john@>
2017-08-15 09:38:11 -0700

    add branching to look for vowels in the inputted sentence

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
-$(".main").submit(function(event){
-  event.preventDefault();
-  alert('Working!');
-
-
-});
-
-
+  $(".main").submit(function(event){
+    event.preventDefault();
+    var userSentence = $("#comment").val();
+    for(var i = 0; i < userSentence.length; i++){
+      if(userSentence[i] === "a"){
+        userSentence[i] = "-";
+      } else if (userSentence[i] === "e") {
+        userSentence[i] = "-";
+      } else if (userSentence[i] === "i") {
+        userSentence[i] = "-";
+      } else if (userSentence[i]) === "o") {
+        userSentence[i] = "-";
+      } else if (userSentence[i] === "u") {
+        userSentence[i] = "-";
+      } else {
+        userSentence[i] = userSentence[i];
+      }
+    };
+    
+  });
 });
