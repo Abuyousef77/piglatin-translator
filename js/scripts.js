4c5eeccfc6f47b11b56108bd4c3b3cdf21075ce7
Dawson Mortenson and John Parsons <dawson@>
2017-08-15 09:54:15 -0700

    finish the javascript logic that loops the loop to replace the vowels with dashes

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,36 @@
 $(document).ready(function(){
   $(".main").submit(function(event){
     event.preventDefault();
+    var vowels = ["a", "e", "i", "o", "u"];
     var userSentence = $("#comment").val();
+
+    userSentence.toLowerCase();
+
     for(var i = 0; i < userSentence.length; i++){
-      if(userSentence[i] === "a"){
-        userSentence[i] = "-";
-      } else if (userSentence[i] === "e") {
-        userSentence[i] = "-";
-      } else if (userSentence[i] === "i") {
-        userSentence[i] = "-";
-      } else if (userSentence[i]) === "o") {
-        userSentence[i] = "-";
-      } else if (userSentence[i] === "u") {
-        userSentence[i] = "-";
-      } else {
-        userSentence[i] = userSentence[i];
-      }
+      for(var k = 0; k < vowels.length; k++){
+        userSentence = userSentence.replace(vowels[k], "-");
+      };
     };
-    
+    $("textarea").hide();
+    $(".outputtext").text(userSentence);
   });
 });
+
+
+
+
+// for(var i = 0; i < userSentence.length; i++){
+//   if(userSentence[i] === "a"){
+//     userSentence = userSentence.replace("a", "-");
+//   } else if (userSentence[i] === "e") {
+//     userSentence = userSentence.replace("a", "-");
+//   } else if (userSentence[i] === "i") {
+//     userSentence = userSentence.replace("a", "-");
+//   } else if (userSentence[i] === "o") {
+//     userSentence = userSentence.replace("a", "-");
+//   } else if (userSentence[i] === "u") {
+//     userSentence = userSentence.replace("a", "-");
+//   } else {
+//     ;
+//   }
+// };
