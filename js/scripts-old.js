7d9c2f64fe855398a7424c682c7298ef3fbe2b19
Benjamin Dressler and John Parsons <john@>
2017-08-16 16:28:48 -0700

    add initial translator business logic and front end logic

--- js/scripts-old.js
+++ js/scripts-old.js
@@ -1,31 +1,26 @@
-
+$(document).ready(function(){
+  $(".main").submit(function(event){
+    event.preventDefault();{
 function isVowel(char) {
 return ['a', 'e', 'i', 'o', 'u'].indexOf(char) != -1;
-};
-
+}
 function consonant(text) {
 i = 0;
-while (text.length >= i && !isVowel(text.charAt(i)))
+while (!isVowel(text.charAt(i)))
 i++;
 return text.substring(i) + text.substring(0, i) + "ay";
-};
-
-
+}
 function vowel(text) {
 if (text.charAt(text.length - 1) == 's')
 return text.substring(0, text.length -1) + "ays";
 return text + "ay";
-};
-
-
+}
 function wordProcessor(text) {
 if (isVowel(text.charAt(0)))
 return vowel(text);
 else
 return consonant(text);
-};
-
-
+}
 function toPigLatin(form) {
 form.output.value = "";
 phrase = form.phrase.value;
@@ -38,13 +33,11 @@ phrase = phrase.substring(i + 1);
 else
 phrase = phrase.substring(i);
 i = -1;
-};
+}
 if (phrase.length == 1) {
 endPhrase += wordProcessor(phrase);
 phrase = "";
-};
-};
-
-
+}
+}
 form.output.value += endPhrase;
-};
+}
